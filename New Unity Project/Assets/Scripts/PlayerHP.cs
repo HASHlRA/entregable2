ad5beb919caed7b82e236271a578b3ed061a2a78
Rowen Lumanlan <rowenlumanlan@gmail.com>
2021-10-25 08:57:48 +0200

    entregable2

--- New Unity Project/Assets/Scripts/PlayerHP.cs
+++ New Unity Project/Assets/Scripts/PlayerHP.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerHP : MonoBehaviour
 {
+    [SerializeField] private bool isgameOver;
+    private int playerHP = 150;
+    public int damage = 60;
+
     // Start is called before the first frame update
     void Start()
     {
+       playerHP = playerHP - damage;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isgameOver == false)
+        {
+            if (playerHP <= 0)
+            {
+                Debug.Log("Te has quedado sin vida… GAME OVER");
+            }
+            else if (playerHP < 30)
+            {
+                Debug.Log(" ¡¡¡Tienes poca vida!!!");
+            }
+            else
+            {
+                Debug.Log("Vas bien de vida: tienes {playerHP} puntos de vida");
+            }
+        }
+        else
+        {
+            Debug.Log("GAME OVER");
+        }
+
         
     }
 }
